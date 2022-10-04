dc89a6ae9d13fcbb2d4ec0d61c763225b310571b
i_4m_h4ck3rm4n <piunger@hotmail.com>
2022-10-04 01:10:48 -0700

    fixed a sysout to use log

--- UI.java
+++ UI.java
@@ -27,7 +27,7 @@ public class UI implements ActionListener {
         root.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         root.setLayout(new GridLayout(2, 1, 5, 5));     // create a gridlayout with 2 rows and 1 column
         root.add(mainButton);
-        root.add(scrollPane);
+        root.add(scrollPane);       // add the scrollPane that the consoleOutput is inside
 
         // frame parameter boilerplate code
         frame.add(root);    // add the main root panel to the frame
@@ -47,11 +47,11 @@ public class UI implements ActionListener {
             log("Button has now been clicked " + count + " times.");
         }
         else {
-            System.out.println("unknown command");
+            log("unknown command");
         }
     }
 
-    public void log(String msg) {
+    public void log(String msg) {   // method to print any error messages/user messages with a timestamp to the consoleOutput
         consoleOutput.setText(consoleOutput.getText() + LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss.S")) + " : " + msg + "\n");
     }
 }
