ca1e1d9897bc7b89e5e8c79f253abf8595ae0adb
i_4m_h4ck3rm4n <piunger@hotmail.com>
2022-10-12 00:02:55 -0700

    minor tweaks

--- InventorySystem.java
+++ InventorySystem.java
@@ -13,25 +13,25 @@ public class InventorySystem {
         ui = new MainUI(new String[][]{{"1", "pencils", "30"}}, new String[]{"id", "name", "currentStock"}, "C:\\Users\\Paul\\Documents\\School\\etcbruhjustgonnapadthisstringsuperlong.csv");
     }
 
-    public static void load() {     // this method is called by the listener in MainUI
+    public static void load() {         // this method is called by the listener in MainUI
         ui.log("load");
     }
-    public static void save() {     // this method is called by the listener in MainUI
+    public static void save() {         // this method is called by the listener in MainUI
         ui.log("save");
     }
-    public static void add() {      // this method is called by the listener in MainUI
+    public static void add() {          // this method is called by the listener in MainUI
         ui.log("add");
     }
-    public static void edit() {     // this method is called by the listener in MainUI
+    public static void edit() {         // this method is called by the listener in MainUI
         ui.log("edit");
     }
-    public static void order() {    // this method is called by the listener in MainUI
+    public static void order() {        // this method is called by the listener in MainUI
         ui.log("order");
     }
-    public static void transaction() {
+    public static void transaction() {  // this method is called by the listener in MainUI
         ui.log("transaction");
     }
-    public static void setTime() {
+    public static void setTime() {      // this method is called by the listener in MainUI
         ui.log("set time");
     }
 }
